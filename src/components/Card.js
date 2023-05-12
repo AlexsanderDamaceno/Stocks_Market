01d9765fc7c1ab5f496d07b21ce5d22d51c3509d
AlexsanderDamaceno <aemgbo@gmail.com>
2023-05-12 04:00:00 -0300

    add searchBar

---
+++ src/components/Card.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+
+
+const Card = ({children}) => {
+
+    return  <div className="w-full h-full bg-white shadow-lg rounded-md">{children}</div>
+};
+
+export default Card; 
